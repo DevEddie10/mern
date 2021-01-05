8c1b947d2c311d3b5f277b03f4637b26f699019c
DESKTOP-8RNQDLO\EDDIE <daniel@gmail.com>
2021-01-05 11:06:14 -0600

    Modificando el puerto y el host

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //puerto de la app
-const port = process.env.port || 4000;
+//const port = process.env.port || 4000;
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 4000;
 
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +27,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 //arrancar la app a production
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, host, () => {
     console.log(`El servidor esta funcionado en el puerto ${port}`);
 });
